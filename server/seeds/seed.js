bb78c7d02b17c1955a3fec1c23695ca627f5579f
michael seaman <mseaman26@gmail.com>
2023-02-20 23:18:38 -0800

    seeded a single item

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,15 @@
 const db = require('../config/connection')
 const { User } = require('../models')
+const Item = require('../models/Item')
 const userSeeds = require('./userSeed.json')
+const inventorySeed = require('./inventorySeed.json')
 
 db.once('open', async () => {
     try{
         await User.deleteMany({})
         await User.create(userSeeds)
-
+        await Item.deleteMany({})
+        await Item.create(inventorySeed)
         console.log('data seeded!')
         process.exit(0)
     }catch(err){
