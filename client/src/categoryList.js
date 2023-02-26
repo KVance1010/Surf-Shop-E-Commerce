c319179fd138f41ead4db87678b3271159db69cd
michael seaman <mseaman26@gmail.com>
2023-02-25 23:59:10 -0800

    moved categories to js file

---
+++ client/src/categoryList.js
@@ -0,0 +1,139 @@
+export const mensApparelCategory = 
+{
+    "name": "Mens Apparel",
+    "categories": [
+        {
+            "name": "Boardshorts",
+            "image": "s/1/2034/7683/products/SP1106012_PBL_P_1_360x.jpg?v=1653426961"
+        },
+        {
+            "name": "Shirts",
+            "image": "https://img.ltwebstatic.com/images3_pi/2022/06/10/165483371841ff8c0b374ce23827774642a2ebd734.webp"
+        },
+        {
+            "name": "Shorts",
+            "image": "https://www.ripcurl.com/media/catalog/product/c/w/cwacz9_0090-1_11.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=950&width=768&canvas=768:950"
+        },
+        {
+            "name": "Hoodies",
+            "image": "https://www.ripcurl.com/media/catalog/product/c/f/cfehk9_0090_2_mf_rollover_update_4.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=950&width=768&canvas=768:950"
+        },
+        {
+            "name": "Foodwear",
+            "image": "https://cdn.shopify.com/s/files/1/0589/8924/8667/products/RF002611BZB_Main_1100x.jpg?v=1642827732"
+        },
+        {
+            "name": "Hats",
+            "image": "https://cdn.watersportsoutlet.com/images/1x1/thumbs/2022%20Mystic%202mm%20Neoprene%20Beanie%2035016.230024%20-%20Dark%20Olive.700x700.jpg"
+        }
+    ]
+}
+export const womensApparelCategory = 
+{
+    "name": "Womens Apparel",
+    "categories": [
+        {
+            "name": "Swimsuits",
+            "image": "https://cdn.shopify.com/s/files/1/0783/3649/products/gsixx9_0090-3_1000x.jpg?v=1647972464"
+        },
+        {
+            "name": "Tops",
+            "image": "https://cdn.shopify.com/s/files/1/1869/0651/products/2_ModelBack_3c9ead6d-eb16-4941-b905-5ff1f1974b07_1024x.jpg"
+        },
+        {
+            "name": "Hoodies",
+            "image": "https://cdn.shopify.com/s/files/1/0721/2253/products/Untitled-5_800x.jpg?v=1672872327"
+        },
+        {
+            "name": "Footwear",
+            "image": "https://cdn.shopify.com/s/files/1/0589/8924/8667/products/RF0A3OKSNAT_MAIN_1100x.jpg?v=1648662655"
+        },
+        {
+            "name": "Hats",
+            "image": "https://cdn.shopify.com/s/files/1/2062/5873/products/ABJHA00222BLKSecondary1_x1400.jpg?v=1674244737"
+        }
+    ]
+}
+export const accessoriesCategory = 
+{
+    "name": "Accessories",
+    "categories": [
+        {
+            "name": "Watches",
+            "image": "https://m.media-amazon.com/images/I/51IA+C-sbWL._AC_UX679_.jpg"
+        },
+        {
+            "name": "Shades",
+            "image": "https://assets.oakley.com/is/image/OakleyEYE/888392593429__PREMIUM__shad__al6.png?impolicy=OO_ratio&width=1000"
+        },
+        {
+            "name": "Towels",
+            "image": "https://cdn.shopify.com/s/files/1/0714/2297/products/AT5I1719.jpg?v=1615418124"
+        },
+        {
+            "nam": "Bags",
+            "image": "https://cdn.shopify.com/s/files/1/0838/7991/products/cotton-tote-bag_1000x.jpg?v=1627997728"
+        }
+    ]
+}
+export const surfboardsCategory = 
+{
+    "name": "Surfboards",
+    "categories": [
+        {
+            "name": "Shortboards",
+            "image": "https://cdn.shopify.com/s/files/1/0003/1902/9309/products/ci_rw_fut_5000x.jpg?v=1623958927"
+        },
+        {
+            "name": "Longboards",
+            "image": "https://assets.bigcartel.com/product_images/290730180/HRS+Surfboard+Velvet+rz+_100_+rz.jpg?auto=format&fit=max&h=1000&w=1000"
+        },
+        {
+            "name": "Hybrids",
+            "image": "https://www.vernorsurfboards.com/img/boards/main-hybrid.jpg"
+        },
+        {
+            "name": "Guns",
+            "image":"https://images3.shaperbuddy.com/18/surfboards/1123169905d099.jpg"
+        }
+    ]
+}
+export const surfAccessoriesCategory = 
+{
+    "name": "Surf Accessories",
+    "categories": [
+        {
+            "name": "Leashes",
+            "image": "https://cdn.shopify.com/s/files/1/0770/3687/products/heavyduty-leash-6.jpg?v=1610501535&width=1024"
+        },
+        {
+            "name": "Wax",
+            "image": "https://cdn11.bigcommerce.com/s-wsye173qvw/images/stencil/1280x1280/products/340/1116/COLD3-22__56610.1657811872.jpg?c=2"
+        },
+        {
+            "name": "Sunscreen",
+            "image": "https://cdn.shopify.com/s/files/1/1916/1271/products/SS-StussySPFStick-Open_1080x.png?v=1643931030"
+        },
+        {
+            "name": "Fins",
+            "image": "https://cdn.shopify.com/s/files/1/0332/4362/7659/products/legacy_rake_xsmall_1800x1800.jpg?v=1656617784"
+        },
+        {
+            "name": "Ding Repair",
+            "image": "https://m.media-amazon.com/images/I/61rzcpOU4ZL._AC_SX679_.jpg"
+        },
+        {
+            "name": "Rashguards",
+            "image": "https://images.boardriders.com/globalGrey/quiksilver-products/all/default/xlarge/eqywr03357_quiksilver,v_byhh_frt1.jpg"
+        },
+        {
+            "name": "Earplugs",
+            "image": "https://cdn.shopify.com/s/files/1/1124/3298/products/61ee44fb-21c9-4f34-ba19-95603ab323bd_1966x1388.png?v=1639548855"
+        },
+        {
+            "name": "Tail Pads",
+            "image": "https://cdn11.bigcommerce.com/s-1pj0bf1gcq/images/stencil/1280x1280/products/533/2189/00ymsh_0090-1_1__94603.1671830752.jpg?c=1"
+        }
+
+    ]
+}
