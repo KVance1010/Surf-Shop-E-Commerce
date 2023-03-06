8354c079077651b53d11400660e9840f06aa2f1c
michael seaman <mseaman26@gmail.com>
2023-03-06 12:51:56 -0800

    addToCart button changes cart state

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { QUERY_ITEM_BY_ID } from '../utils/queries'
 import  {QUERY_ITEM_BY_NAME}  from '../utils/queries'
+import { useCartContext } from '../utils/cartContext'
 
 
 
@@ -11,9 +12,10 @@ const Item = () => {
     
     const {item} = useParams({})
     
-    
-    
-    // const {loading, data} = useQuery(QUERY_ITEM_BY_ID, {
+    const { cart, addItem, clearCart, cartTotal } =  useCartContext()
+    console.log(cart)
+    console.log(cartTotal())
+     // const {loading, data} = useQuery(QUERY_ITEM_BY_ID, {
     //     variables: {_id: item}
     // })
     const {loading, data} = useQuery(QUERY_ITEM_BY_NAME, {
@@ -64,8 +66,10 @@ const Item = () => {
                             </h1>
                             
                             <div className='btn btn-success' onClick={() => {
-                            }
-                                
+                                addItem({name: item})
+                                console.log(cart)
+                                alert(`${item} has been added to your cart!`)
+                            }   
                             }>
                                 Add to Cart!
                             </div>
