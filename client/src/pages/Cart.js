30de7f8a80034a4e1913647624771b7c93840af5
michael seaman <mseaman26@gmail.com>
2023-03-08 10:50:49 -0800

    cart page does basic breakdown of items

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -6,11 +6,36 @@ import { useCartContext } from "../utils/cartContext";
 const Cart = () => {
     const { cart, setCart, clearCart, addItem, removeItem, cartTotal} = useCartContext()
 
+    const itemNameList = []
+    for(let key in cart){
+        itemNameList.push(key)
+    }
+    console.log(itemNameList)
+
+    const {loading, data} = useQuery(QUERY_ITEMS_BY_NAMES, {
+        variables: {names: itemNameList}
+    })
+
+    const items = data?.itemsByNames || []
+    console.log(items)
+    console.log('cart', cart)
+
     return(
         <div>
-            <ul>
-                
-            </ul>
+            {loading ? (
+                <div>
+                    ...Loading
+                </div>
+            ) : (
+                <ul>
+                    {items.map((item) => 
+                        <h3>
+                            {`${item.name}: Quantity: ${cart[item.name]}`}
+                        </h3>
+                    )}
+                </ul>
+            )}
+
         </div>
     )
 }
--- client/src/utils/cartContext.js
+++ client/src/utils/cartContext.js
@@ -5,7 +5,7 @@ const CartContext = createContext()
 export const useCartContext = () => useContext(CartContext)
 
 export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState({surfboard: 1})
+    const [cart, setCart] = useState({})
 
     const addItem = (item) => {
         let newCart = cart
