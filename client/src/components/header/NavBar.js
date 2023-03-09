4d6aab125035f857c70187acab586a5aa82e9b5e
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-03-09 12:03:13 -0800

    fixed women in search bar

--- client/src/components/header/NavBar.js
+++ client/src/components/header/NavBar.js
@@ -1,41 +1,54 @@
-import React, { useEffect, useState, useContext} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import cartIcon from '../../images/cartIcon.svg';
 import '../../css/header/NavBar.css';
+import PersonIcon from '@mui/icons-material/Person';
 import { useCartContext } from '../../utils/cartContext';
 
-
 const Links = ({ currentPage, handlePageChange }) => {
-	
-	const { cart, cartTotal } = useCartContext()
-	
-	
+	const { cart, cartTotal } = useCartContext();
+
 	return (
 		<>
 			<li>
-				<Link to="/apparel" className="navLink" onClick = {(event)=>handlePageChange(event.target.value)}>
+				<Link
+					to="/apparel"
+					className="navLink"
+					onClick={(event) => handlePageChange(event.target.value)}
+				>
 					Apparel
 				</Link>
 			</li>
 			<li>
-				<Link to="/surfboards/surfboards" className="navLink"
-				 onClick = {(event)=>handlePageChange(event.target.value)}>
+				<Link
+					to="/surfboards/surfboards"
+					className="navLink"
+					onClick={(event) => handlePageChange(event.target.value)}
+				>
 					Surfboards
 				</Link>
 			</li>
 			<li>
-				<Link to="/accessories/accessories" className="navLink"
-				 onClick = {(event)=>handlePageChange(event.target.value)}>
+				<Link
+					to="/accessories/accessories"
+					className="navLink"
+					onClick={(event) => handlePageChange(event.target.value)}
+				>
 					Accessories
 				</Link>
 			</li>
 			<li>
-				<Link to="/cart" className="navLink"
-				 onClick = {(event)=>handlePageChange(event.target.value)}>
-					<img src={cartIcon} className= 'cartIcon'alt='cart icon' />
-					
+				<Link
+					to="/cart"
+					className="navLink"
+					onClick={(event) => handlePageChange(event.target.value)}
+				>
+					<img src={cartIcon} className="cartIcon" alt="cart icon" />
 				</Link>
 				<div>{cartTotal()}</div>
+			</li>
+			<li>
+				<PersonIcon />
 			</li>
 		</>
 	);
