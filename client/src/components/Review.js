f862401360fa8e13bed8b576ab351626e65c1510
michael seaman <mseaman26@gmail.com>
2023-03-09 13:04:31 -0800

    review page shows totals

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -4,6 +4,9 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
+import { useCartContext } from '../utils/cartContext';
+import { useQuery } from '@apollo/client';
+import { QUERY_ITEMS_BY_NAMES } from '../utils/queries';
 
 const products = [
   {
@@ -38,52 +41,71 @@ const payments = [
 ];
 
 export default function Review() {
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Order summary
-      </Typography>
-      <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
-          </ListItem>
-        ))}
 
-        <ListItem sx={{ py: 1, px: 0 }}>
-          <ListItemText primary="Total" />
-          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
-          </Typography>
-        </ListItem>
-      </List>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Shipping
-          </Typography>
-          <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
-        </Grid>
-        <Grid item container direction="column" xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Payment details
-          </Typography>
-          <Grid container>
-            {payments.map((payment) => (
-              <React.Fragment key={payment.name}>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.name}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.detail}</Typography>
-                </Grid>
-              </React.Fragment>
+    const { cart } = useCartContext()
+
+    const itemNameList = []
+    for(let key in cart){
+        itemNameList.push(key)
+    }
+
+    const { loading, data } = useQuery(QUERY_ITEMS_BY_NAMES, {
+        variables: {names: itemNameList}
+    })
+
+    const items = data?.itemsByNames || []
+
+    let total = 0
+    for(let i = 0; i < items.length; i++){
+        total += items[i].price * cart[items[i].name]
+    }
+
+    return (
+        <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+            Order summary
+        </Typography>
+        <List disablePadding>
+            {items.map((item) => (
+            <ListItem key={item.name} sx={{ py: 1, px: 0 }}>
+                <ListItemText primary={`${item.name} X ${cart[item.name]}` }  />
+                <Typography variant="body2">{item.price * cart[item.name]}</Typography>
+            </ListItem>
             ))}
-          </Grid>
+
+            <ListItem sx={{ py: 1, px: 0 }}>
+            <ListItemText primary="Total" />
+            <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+                {total}
+            </Typography>
+            </ListItem>
+        </List>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+            <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                Shipping
+            </Typography>
+            <Typography gutterBottom>John Smith</Typography>
+            <Typography gutterBottom>{addresses.join(', ')}</Typography>
+            </Grid>
+            <Grid item container direction="column" xs={12} sm={6}>
+            <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                Payment details
+            </Typography>
+            <Grid container>
+                {payments.map((payment) => (
+                <React.Fragment key={payment.name}>
+                    <Grid item xs={6}>
+                    <Typography gutterBottom>{payment.name}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <Typography gutterBottom>{payment.detail}</Typography>
+                    </Grid>
+                </React.Fragment>
+                ))}
+            </Grid>
+            </Grid>
         </Grid>
-      </Grid>
-    </React.Fragment>
-  );
-}
+        </React.Fragment>
+    );
+    }
