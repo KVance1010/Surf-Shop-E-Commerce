b0f55bf340a43b3577cdaad0c1fd74f3aeff071e
michael seaman <mseaman26@gmail.com>
2023-03-24 18:39:31 -0700

    still trying to get it to not crash

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -44,7 +44,7 @@ const OrderHistory = () => {
                     id: id
                 }
             })
-            window.location.replace('/account')
+            window.location.replace('/')
         }
         
     }
